d7d2759125dec3fd9b5ae90872bd2bf234f1f13d
Nathaniel Cowan <nathaniel.g.cowan@gmail.com>
2025-07-09 16:19:43 -0500

    worked on normal functions

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,12 @@ import './App.css'
 
 function App() {
 
-  function newFunction(name, callback) {
-    callback("Hello " + name)
-  }
-
-  function printVariable(variable) {
-    console.log(variable)
-  }
-
-  // Create a new function that takes two parameters.
-  // 1. name
-  // 2. callback that prints out what we pass to it(printVaribale)
-  // Take name and append "Hello" to the beginning of the name
-  // If I pass in "Kyle" It will pirnt "Hello Kyle"
+  
 
   return (
    <div className="App">
     {
-      newFunction("Nathaniel", printVariable)
+      
     }
     </div>
   )
