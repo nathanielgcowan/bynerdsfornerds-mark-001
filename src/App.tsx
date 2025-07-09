b59845737b3251c4b93724e1d958e73dc53cfdd3
Nathaniel Cowan <nathaniel.g.cowan@gmail.com>
2025-07-09 16:10:28 -0500

    this is test content.

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,25 @@ import './App.css'
 
 function App() {
 
-  function setname(name) {
-    return "Hello" + name
+  function newFunction(name, callback) {
+    callback("Hello " + name)
   }
 
+  function printVariable(variable) {
+    console.log(variable)
+  }
+
+  // Create a new function that takes two parameters.
+  // 1. name
+  // 2. callback that prints out what we pass to it(printVaribale)
+  // Take name and append "Hello" to the beginning of the name
+  // If I pass in "Kyle" It will pirnt "Hello Kyle"
 
   return (
-    <div>
-      {setname("Nathaniel")}
+   <div className="App">
+    {
+      newFunction("Nathaniel", printVariable)
+    }
     </div>
   )
 }
